4d0ada0ac821a098ee8f8882f7a4e45913294e6d
lemon4ex <netchen@vip.qq.com>
2019-04-01 13:19:04 +0800

    Initial Commit

---
+++ UCPure/UCPure.h
@@ -0,0 +1,9 @@
+//
+//  UCPure.h
+//  UCPure
+//
+//  Created by h4ck on 2019/4/1.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ UCPure/UCPure.m
@@ -0,0 +1,15 @@
+//
+//  UCPure.m
+//  UCPure
+//
+//  Created by h4ck on 2019/4/1.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UCPure.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ UCPure loaded ++++++++");
+}
